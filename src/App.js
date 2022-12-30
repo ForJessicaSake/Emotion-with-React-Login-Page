e5442494c7bf1d2a194b5b95a9f68ff017b0ecec
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-12-30 09:33:58 +0100

    fix:media queries completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import the styled package from emotion/styled
 import styled from "@emotion/styled";
+
 import bus from "./Asserts/Illustration.svg";
 import logo from "./Asserts/Logo.svg";
 
@@ -7,11 +8,15 @@ function App() {
   const MainContent = styled.div`
     display: flex;
     justify-content: space-between;
+    @media (max-width: 1024px) {
+      flex-direction: column-reverse;
+    }
   `;
   const Nav = styled.div`
     display: flex;
     justify-content: space-between;
     width: 160px;
+    padding:2% 0%
   `;
   const BrandName = styled.h1`
     color: #355b3e;
@@ -19,7 +24,7 @@ function App() {
     font-weight: 700;
   `;
   const Text = styled.div`
-    padding: 3% 3%;
+    padding: 0% 3%;
   `;
 
   const HeaderText = styled.h3`
@@ -27,6 +32,9 @@ function App() {
     font-size: 19px;
     font-weight: 700;
     margin-top: 5%;
+    @media (max-width: 1024px) {
+      font-size: 19px;
+    }
   `;
   const HeaderWelcomeMessage = styled.p`
     color: #58745e;
@@ -41,7 +49,7 @@ function App() {
     color: #355b3e;
     font-size: 14px;
     font-weight: 600;
-    margin-bottom:2%;
+    margin-bottom: 2%;
   `;
   const Input = styled.input`
     width: 422px;
@@ -50,64 +58,98 @@ function App() {
     border-radius: 8px;
     margin-bottom: 5%;
     padding: 0% 2%;
+    @media (max-width: 1024px) {
+      width: 350px ;
+    }
   `;
   const Options = styled.div`
-  display:flex;
-  justify-content:space-between;
-  align-items: center;
-  color: #355b3e;
-  font-size:14.5px;
-  `
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #355b3e;
+    font-size: 14.5px;
+    @media (max-width: 1024px) {
+      font-size: 14px;
+      width:360px
+    }
+  `;
   const CheckboxInput = styled.input`
-  background-color:#355b3e;
-  margin-top:2%;
-  width:14px;
-  height:14px
-  `
+    background-color: #355b3e;
+    margin-top: 2%;
+    width: 14px;
+    height: 14px;
+  `;
   const ForgotPassword = styled.p`
-  text-decoration: underline;
-
-  `
+    text-decoration: underline;
+  `;
   const CallToAction = styled.div`
-  display:flex;
-  justify-content:space-between
-  `
+    display: flex;
+    justify-content: space-between;
+    @media (max-width: 1024px) {
+      width:360px
+    }
+  `;
 
   const Button = styled.button`
-  width:180px;
-  height:48px;
-  border-radius:8px;
-  border:0px;
-  margin-top:8%;
-  background-color:white;
-  color:#029664;
-  border: 1px solid #029664;
-  `
+    width: 180px;
+    height: 48px;
+    border-radius: 8px;
+    border: 0px;
+    margin-top: 8%;
+    background-color: white;
+    color: #029664;
+    border: 1px solid #029664;
+    @media (max-width: 1024px) {
+      width: 160px;
+    }
+  `;
   const ButtonLogin = styled.button`
-  width:180px;
-  height:48px;
-  border-radius:8px;
-  border:0px;
-  margin-top:8%;
-  background-color:#029664;
-  color:white;
+    width: 180px;
+    height: 48px;
+    border-radius: 8px;
+    border: 0px;
+    margin-top: 8%;
+    background-color: #029664;
+    color: white;
+    @media (max-width: 1024px) {
+      width: 160px;
+    }
+  `;
+  const UnorderedList = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5% 0%;
+    font-size: 14.5px;
+    color: #355b3e;
+    @media (max-width: 1024px) {
+      width: 360px;
+    }
+  `;
+  const List = styled.li`
+    list-style: none;
+    font-weight: 600;
+  `;
+
+  const Listparagraph = styled.p`
+  @media (max-width: 1024px) {
+    display: none;
+  }
   `
-const UnorderedList = styled.ul`
-display:flex;
-justify-content:space-between;
-align-items:center;
-padding:5% 0%;
-font-size:14.5px;
-color: #355b3e;
-`
-const List = styled.li`
-list-style:none;
-font-weight:600;
-`
+  const Image = styled.img`
+  width:100%;
+  height:100%;
+  @media (max-width:1024px) {
+    width:100%;
+    height:10%;
+  }
+  `
 
   return (
+
     <MainContent>
       <Text>
+
         <Nav>
           <img src={logo} alt="Logo" />
           <BrandName>Travalizer</BrandName>
@@ -123,36 +165,34 @@ font-weight:600;
         </header>
         <Form>
           <Label>Email</Label>
-          <Input required type="email" placeholder="enter your email" />
+          <Input required type="email" placeholder="enter your email"/>
           <Label>Password</Label>
           <Input required type="password" placeholder="enter your password" />
           <Options>
-          <label>
-          <CheckboxInput
-            type='checkbox'
-            />
-            Remember me
-          </label>
+            <label>
+              <CheckboxInput type="checkbox" />
+              Remember me
+            </label>
             <ForgotPassword>Forgot password?</ForgotPassword>
           </Options>
           <CallToAction>
-          <ButtonLogin>Login</ButtonLogin>
-          <Button>Sign Up</Button>
+            <ButtonLogin>Login</ButtonLogin>
+            <Button>Sign Up</Button>
           </CallToAction>
         </Form>
 
         <footer>
-        <UnorderedList>
-           <p>or login with </p>
+          <UnorderedList>
+            <Listparagraph>or login with </Listparagraph>
             <List>Facebook</List>
             <List>Linked In</List>
             <List>Google</List>
-           </UnorderedList>
+          </UnorderedList>
         </footer>
       </Text>
 
       <section className="image">
-        <img src={bus} alt="a bus" />
+        <Image src={bus} alt="a bus"/>
       </section>
     </MainContent>
   );
