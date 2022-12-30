3fc2894f2ca9072c8e600001dbd4dee9603c766f
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-12-30 10:46:46 +0100

    fix:added the cursor attribute to the input fields and links

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 //import the styled package from emotion/styled
-import styled from "@emotion/styled";
+import styled from '@emotion/styled'
 
-import bus from "./Asserts/Illustration.svg";
-import logo from "./Asserts/Logo.svg";
+import bus from './Asserts/Illustration.svg'
+import logo from './Asserts/Logo.svg'
 
 function App() {
-  const MainContent = styled.div`
+  const MainContent = styled.main`
     display: flex;
     justify-content: space-between;
     @media (max-width: 1024px) {
       flex-direction: column-reverse;
     }
-  `;
-  const Nav = styled.div`
+  `
+  const Nav = styled.nav`
     display: flex;
     justify-content: space-between;
     width: 160px;
-    padding:2% 0%
-  `;
+    padding: 0% 0%;
+  `
   const BrandName = styled.h1`
     color: #355b3e;
     font-size: 22px;
     font-weight: 700;
-  `;
-  const Text = styled.div`
+  `
+  const Text = styled.section`
     padding: 0% 3%;
-  `;
+  `
 
   const HeaderText = styled.h3`
     color: #355b3e;
@@ -35,22 +35,22 @@ function App() {
     @media (max-width: 1024px) {
       font-size: 19px;
     }
-  `;
+  `
   const HeaderWelcomeMessage = styled.p`
     color: #58745e;
     font-size: 13px;
-  `;
+  `
   const Form = styled.div`
     display: grid;
     grid-template-columns: repeat(1, 1fr);
-  `;
+  `
 
   const Label = styled.label`
     color: #355b3e;
     font-size: 14px;
     font-weight: 600;
     margin-bottom: 2%;
-  `;
+  `
   const Input = styled.input`
     width: 422px;
     height: 46px;
@@ -58,12 +58,12 @@ function App() {
     border-radius: 8px;
     margin-bottom: 5%;
     padding: 0% 2%;
-    cursor:text;
+    cursor: text;
     @media (max-width: 1024px) {
-      width: 345px ;
+      width: 345px;
     }
-  `;
-  const Options = styled.div`
+  `
+  const Options = styled.section`
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -71,26 +71,26 @@ function App() {
     font-size: 14.5px;
     @media (max-width: 1024px) {
       font-size: 14px;
-      width:355px
+      width: 355px;
     }
-  `;
+  `
   const CheckboxInput = styled.input`
     background-color: #355b3e;
     margin-top: 2%;
     width: 14px;
     height: 14px;
-    cursor:pointer;
-  `;
+    cursor: pointer;
+  `
   const ForgotPassword = styled.p`
     text-decoration: underline;
-  `;
-  const CallToAction = styled.div`
+  `
+  const CallToAction = styled.section`
     display: flex;
     justify-content: space-between;
     @media (max-width: 1024px) {
-      width:355px
+      width: 355px;
     }
-  `;
+  `
 
   const Button = styled.button`
     width: 180px;
@@ -101,11 +101,11 @@ function App() {
     background-color: white;
     color: #029664;
     border: 1px solid #029664;
-    cursor:pointer;
+    cursor: pointer;
     @media (max-width: 1024px) {
       width: 160px;
     }
-  `;
+  `
   const ButtonLogin = styled.button`
     width: 180px;
     height: 48px;
@@ -114,11 +114,11 @@ function App() {
     margin-top: 8%;
     background-color: #029664;
     color: white;
-    cursor:pointer;
+    cursor: pointer;
     @media (max-width: 1024px) {
       width: 160px;
     }
-  `;
+  `
   const UnorderedList = styled.ul`
     display: flex;
     justify-content: space-between;
@@ -126,35 +126,33 @@ function App() {
     padding: 5% 0%;
     font-size: 14.5px;
     color: #355b3e;
-    cursor:pointer;
+    cursor: pointer;
     @media (max-width: 1024px) {
       width: 355px;
     }
-  `;
+  `
   const List = styled.li`
     list-style: none;
     font-weight: 600;
-  `;
+  `
 
   const Listparagraph = styled.p`
-  @media (max-width: 1024px) {
-    display: none;
-  }
+    @media (max-width: 1024px) {
+      display: none;
+    }
   `
   const Image = styled.img`
-  width:100%;
-  height:100%;
-  @media (max-width:1024px) {
-    width:100%;
-    height:10%;
-  }
+    width: 100%;
+    @media (max-width: 1024px) {
+      width: 100%;
+      height: 10%;
+    }
   `
 
   return (
-
     <MainContent>
-      <Text>
 
+      <Text>
         <Nav>
           <img src={logo} alt="Logo" />
           <BrandName>Travalizer</BrandName>
@@ -168,11 +166,13 @@ function App() {
             Welcome Back, Please login to your account
           </HeaderWelcomeMessage>
         </header>
+
         <Form>
           <Label>Email</Label>
-          <Input required type="email" placeholder="enter your email"/>
+          <Input required type="email" placeholder="enter your email" />
           <Label>Password</Label>
           <Input required type="password" placeholder="enter your password" />
+
           <Options>
             <label>
               <CheckboxInput type="checkbox" />
@@ -180,6 +180,7 @@ function App() {
             </label>
             <ForgotPassword>Forgot password?</ForgotPassword>
           </Options>
+
           <CallToAction>
             <ButtonLogin>Login</ButtonLogin>
             <Button>Sign Up</Button>
@@ -193,13 +194,14 @@ function App() {
             <List>Linked In</List>
             <List>Google</List>
           </UnorderedList>
-        </footer>
+        </footer>  
       </Text>
 
-      <section className="image">
-        <Image src={bus} alt="a bus"/>
+      <section>
+        <Image src={bus} alt="a bus" />
       </section>
+
     </MainContent>
-  );
+  )
 }
-export default App;
+export default App
